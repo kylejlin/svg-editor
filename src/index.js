e50a9a8f636d7ffed8c3a280846648ad7c7b3bb3
Kyle Lin <kylejlin.us@gmail.com>
2018-04-15 21:09:21 -0700

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import SVGEditor from './SVGEditor';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<SVGEditor />, document.getElementById('root'));
-registerServiceWorker();
--- src/svgStarter.js
+++
@@ -1,3 +0,0 @@
-export default `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-  <circle cx="50" cy="50" r="50" fill="#fa0"/>
-</svg>`
